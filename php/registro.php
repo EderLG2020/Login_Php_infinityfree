65816658dc1367fa6f7b34a570608a6df8e09d59
EderLG2021 <edermaster12@hotmail.com>
2023-01-05 12:35:15 -0500

    registro y  validado

---
+++ php/registro.php
@@ -0,0 +1,16 @@
+<?php
+error_reporting(0);
+$nameuser = $_POST['nameuser'];
+$correoe = $_POST['correoe'];
+$numerotelefonico = $_POST['numerotelefonico'];
+$direccion = $_POST['direccion'];
+$apodo = $_POST['apodo'];
+$pass1 = $_POST['pass1'];
+// $Bs= mysqli_connect("localhost","root","ederllancari12","usuario") or die("No se conecto a la base de datos");
+
+$Bs= mysqli_connect("sql205.epizy.com","epiz_33168732","E38VWnBU6w7hd","epiz_33168732_usuario") or die("No se conecto a la base de datos");
+$Bs -> set_charset("utf8");
+$Inser = "INSERT INTO usuario(nameuser,correo,numerotelefonico,direccion,apodo,pass1) VALUES('$nameuser', '$correoe','$numerotelefonico','$direccion','$apodo','$pass1')";
+  mysqli_query($Bs, $Inser);
+  header("Location: ../view/principal.html");
+?>
---
+++ php/validar.php
@@ -0,0 +1,23 @@
+<?php
+$dbhost = "sql205.epizy.com";
+$dbuser = "epiz_33168732";
+$dbpass = "E38VWnBU6w7hd";
+$dbname = "epiz_33168732_usuario";
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+	if (!$conn) {
+			die("No hay conexión: ".mysqli_connect_error());
+		}
+	$nombre = $_POST["usuario"];
+	$pass = $_POST["pass"];
+	$query = mysqli_query($conn,"SELECT * FROM usuario WHERE nombre= '".$nombre."' and contrasenia = '".$pass."'");
+	$nr = mysqli_num_rows($query);
+		if($nr == 1)
+			{
+				echo "<script>alert('Bienvenido') </script>";
+				header("Location: ../view/principal.html");
+			}
+		else if ($nr == 0) 
+			{
+				echo "<script>alert('No tiene acceso') </script>";
+		}
+?>
